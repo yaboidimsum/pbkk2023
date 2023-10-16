ec82328944f75c033751baadd3bbfda2055311c3
Dimas Prihady Setyawan <101172637+yaboidimsum@users.noreply.github.com>
2023-10-16 10:38:02 +0700

    Add files via upload

---
+++ MAUISplashScreen/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiSplashScreen
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
